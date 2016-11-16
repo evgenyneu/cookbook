b3335075a3d6a5289fa82a4b8985d1e7a6db1104
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2016-11-17 10:32:30 +1100

    Validate recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,6 @@
 class Recipe < ApplicationRecord
+  validates_presence_of :name, :instructions, :cuisine, :cooking_time
+  validates_length_of :name, :cuisine, maximum: 100
+  validates_length_of :instructions, maximum: 1000
+  validates :cooking_time, numericality: { greater_than: 0, only_integer: true }
 end
